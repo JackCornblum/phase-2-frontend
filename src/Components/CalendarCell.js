1fb375b42184355250f04873f3c6e8af51f7f1ae
jackcornblum <jcornblum@gmail.com>
2021-06-22 15:57:09 -0400

    add meal functionality implemented

--- src/Components/CalendarCell.js
+++ src/Components/CalendarCell.js
@@ -3,11 +3,13 @@ import 'reactjs-popup/dist/index.css';
 import {useState} from 'react';
 import FoodItem from './FoodItem'
 import {Button} from "react-bootstrap"
+import { auto } from '@popperjs/core';
 
 function CalendarCell({id}){
 
     const [searchValue, setSearchValue] = useState("")
     const [resultState, setResultState] = useState([])
+    const [popupRendered, setPopupRendered] = useState(true)
 
     function handleChange(e) {
         setSearchValue(e.target.value)
@@ -21,7 +23,7 @@ function CalendarCell({id}){
         .then(data => {
             let foodResults = data.results.map(obj => {
                 return (
-                    <FoodItem title={obj.title} image={obj.image} id={obj.id} />
+                    <FoodItem setResultState={setResultState} setSearchValue={setSearchValue} popupRendered={popupRendered} renderRecipe={renderRecipe} setPopupRendered={setPopupRendered} title={obj.title} image={obj.image} id={obj.id} key={obj.id} />
                 )
             })
             console.log(foodResults)
@@ -29,17 +31,48 @@ function CalendarCell({id}){
         })
     }
 
+    function renderRecipe(id){
+        fetch(`https://api.spoonacular.com/recipes/${id}/information?apiKey=9408d54a6e154e6abc0e74e0d986d24b&includeNutrition=true`)
+        .then(resp => resp.json())
+        .then(data => {
+            let recipeDetails = <FoodItem setSearchValue={setSearchValue} setResultState={setResultState} setPopupRendered={setPopupRendered} title={data.title} image={data.image} recipe={data.sourceUrl} key={data.id} />
+            setResultState(recipeDetails)
+        })
+
+    }
+
+    function handleClick(){
+        setResultState([])
+        setSearchValue("")
+        console.log("hello")
+    }
+
+    let popupJsx = (
+        <Popup onClose={handleClick} contentStyle={{width:auto}} trigger={<button  variant="success" size="sm"> Add Meal</button>} position="right center">
+            <form onSubmit={handleSubmit}>
+                <input type="text" value={searchValue} onChange={handleChange}></input>
+            </form>
+
+            {resultState}
+                
+        </Popup>
+    )
+
+
 
     return (
         <td id={id}>
-            <Popup trigger={<Button variant="success" size="sm"> Add Meal</Button>} position="right center">
+            {/* <Popup contentStyle={{width:auto}} trigger={<Button variant="success" size="sm"> Add Meal</Button>} position="right center">
                 <form onSubmit={handleSubmit}>
                     <input type="text" value={searchValue} onChange={handleChange}></input>
                 </form>
 
                 {resultState}
                 
-            </Popup>
+            </Popup> */}
+            {popupRendered ? popupJsx : resultState }
+        
+
         </td>
     )
 }
--- src/Components/FoodItem.js
+++ src/Components/FoodItem.js
@@ -1,10 +1,34 @@
+import { Button } from "bootstrap";
 
 
-function FoodItem({title, image, id}) {
+function FoodItem({title, image, id, setPopupRendered, renderRecipe, popupRendered, setResultState, setSearchValue}) {
+    
+
+    function handleClick(e){
+        if(e.target.textContent === "Add to Planner"){
+            let id = e.target.id
+            setPopupRendered(false)
+            renderRecipe(id)
+            setResultState([])
+            setSearchValue("")
+
+        }
+        else{
+            setPopupRendered(true)
+            setResultState([])
+            setSearchValue("")
+        }
+        // console.log(e)
+    }
+
+
     return (
         <div>
             <h3>{title}</h3>
             <img src={image} />
+            <button onClick={handleClick} id={id} size="sm" >{popupRendered ? "Add to Planner" : "Remove"}</button>
+            
+            
         </div>
     )
 }
